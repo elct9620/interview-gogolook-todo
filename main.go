55d5a8e136e7991dea3d31492282aa7d0d55675f
蒼時弦也 <elct9620@frost.tw>
2023-01-19 15:20:24 +0800

    Setup gin

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := gin.Default()
+
+	r.GET("/tasks", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"result": []gin.H{
+				{"id": 1, "name": "name", "status": 0},
+			},
+		})
+	})
+
+	r.Run()
 }
